6eeffa9a8b56bbb5007b2cdf1917caf665a749a0
Vetty <vetty88@gmail.com>
2020-07-11 17:15:04 +1000

    updated directory; git commit -m"updated direcotry""

---
+++ js.js
@@ -0,0 +1,116 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "project",
+      message: "What is the title of your project??"
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "What does your project do? i.e: how would you describe it?"
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "how do you install your project? what programs did you use to create and run this project?"
+    },
+    {
+      type: "input",
+      name: "usage ",
+      message: "What is your project used for? what is an ideal application?"
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "What are your instructions for gitHub users who would like to pull and/or make changes to this project?"
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "What testing did you do, are their any known issues with this version of the project"
+
+    },
+    {
+      type: "choice",
+      name: "licence",
+      message: "What licence is used?"
+      
+    },
+    {
+      type: "input",
+      name: "github user",
+      message: "What is your github user name?"
+
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?"
+
+    }
+
+
+
+  ]);
+};
+
+function generateTXT(answers) {
+  return `
+## ${answers.title}
+
+  ${answers.description}
+
+## Installation
+
+${answers.installation}
+
+## Usage
+
+${answers.usage}
+
+## Contributing
+
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+
+## License
+${answers.choice}
+// Badge
+
+## Contact
+${answers.email}
+
+## Creator
+${answers.user}`;
+}
+
+promptUser()
+  .then(function(answers) {
+    const txt = generateTXT(answers);
+
+    return writeFileAsync("readme.txt", txt);
+  })
+  .then(function() {
+    console.log("Successfully wrote to readme.txt");
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
+
+
+// async function init() {
+//   console.log("hi")}
+  // try {
+  //   const answers = await promptUser();
+  // }
---
+++ readme.txt
@@ -0,0 +1,30 @@
+
+## undefined
+
+  Being a winner
+
+## Installation
+
+to win
+
+## Usage
+
+undefined
+
+## Contributing
+
+win more
+
+## Tests
+i won at it
+
+
+## License
+undefined
+// Badge
+
+## Contact
+winner.com
+
+## Creator
+undefined
