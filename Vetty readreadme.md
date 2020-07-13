41cc5c132a81427504dc76d2543074b1d0f7615e
Vetty <vetty88@gmail.com>
2020-07-13 22:57:04 +1000

    read me generator with descriptive text

---
+++ Vetty readreadme.md
@@ -0,0 +1,86 @@
+
+
+ <!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+* [About the Project](#answers.project)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [License](#license)
+* [Contact](#contact)
+* [Creator & Acknowledgements](#acknowledgements)
+
+<h1> ## Vetty read </h1>
+
+  read
+
+  [![Project Name Screen Shot][project-screenshot]](https://example.com)
+
+
+## Installation
+To get a local copy up and running follow these simple steps.
+
+read
+
+Prerequisites
+This is an example of how to list things you need to use the software and how to install them.
+
+  npm
+  npm install npm@latest -g
+
+-Installation-
+Clone the repo
+  git clone https://github.com/github_username/repo.git
+Install NPM packages
+  npm install
+
+
+
+## Usage
+
+This project can be used for undefined. Additional screenshots, code examples and demos work well in this space. You may also link to more resources.
+
+For more examples, please refer to the Documentation
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Regarding this project I respectfully make the following requests regarding contributions:
+
+
+read
+
+-- Fork the Project -- 
+Create your Feature Branch 
+  (git checkout -b feature/AmazingFeature)
+Commit your Changes 
+  (git commit -m 'Add some AmazingFeature')
+Push to the Branch 
+  (git push origin feature/AmazingFeature)
+Open a Pull Request
+
+## Tests
+
+See the open issues for a list of proposed features (and known issues). Testing completed and any known issues include the following:
+ read
+
+
+## License
+
+MIT
+
+## Contact
+
+Your Name - vet
+
+Project Link: https://github.com/github_username/repo
+
+## Creator and Acknowledgements
+
+vett
+
+--Acknowledgements
+* []()
+* []()
+* []()
--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function promptUser() {
     },
     {
       type: "input",
-      name: "github user",
+      name: "user",
       message: "What is your github user name?",
 
     },
@@ -67,34 +67,94 @@ function promptUser() {
 
 function generateMD(answers, licenseBadge) {
   return `
-## ${answers.project}
+
+ <!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+* [About the Project](#answers.project)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [License](#license)
+* [Contact](#contact)
+* [Creator & Acknowledgements](#acknowledgements)
+
+<h1> ## ${answers.project} </h1>
 
   ${answers.description}
 
+  [![Project Name Screen Shot][project-screenshot]](https://example.com)
+
+
 ## Installation
+To get a local copy up and running follow these simple steps.
 
 ${answers.installation}
 
+Prerequisites
+This is an example of how to list things you need to use the software and how to install them.
+
+  npm
+  npm install npm@latest -g
+
+-Installation-
+Clone the repo
+  git clone https://github.com/github_username/repo.git
+Install NPM packages
+  npm install
+
+
+
 ## Usage
 
-${answers.usage}
+This project can be used for ${answers.usage}. Additional screenshots, code examples and demos work well in this space. You may also link to more resources.
+
+For more examples, please refer to the Documentation
 
 ## Contributing
 
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Regarding this project I respectfully make the following requests regarding contributions:
+
+
 ${answers.contributing}
 
+-- Fork the Project -- 
+Create your Feature Branch 
+  (git checkout -b feature/AmazingFeature)
+Commit your Changes 
+  (git commit -m 'Add some AmazingFeature')
+Push to the Branch 
+  (git push origin feature/AmazingFeature)
+Open a Pull Request
+
 ## Tests
-${answers.tests}
+
+See the open issues for a list of proposed features (and known issues). Testing completed and any known issues include the following:
+ ${answers.tests}
 
 
 ## License
+
 ${answers.license}
 
 ## Contact
-${answers.email}
 
-## Creator
-${answers.user}`;
+Your Name - ${answers.email}
+
+Project Link: https://github.com/github_username/repo
+
+## Creator and Acknowledgements
+
+${answers.user}
+
+--Acknowledgements
+* []()
+* []()
+* []()
+`;
+
+
 }
 
 // Generates license badge using user's answers from promptUser function
@@ -128,186 +188,6 @@ promptUser()
     
   });
 
-//   Here's a blank template to get started: To avoid retyping too much info. Do a search and replace with your text editor for the following: github_username, repo, twitter_handle, email
-
-// Built With
-// Getting Started
-// To get a local copy up and running follow these simple steps.
-
-// Prerequisites
-// This is an example of how to list things you need to use the software and how to install them.
-
-// npm
-// npm install npm@latest -g
-// Installation
-// Clone the repo
-// git clone https://github.com/github_username/repo.git
-// Install NPM packages
-// npm install
-// Usage
-// Use this space to show useful examples of how a project can be used. Additional screenshots, code examples and demos work well in this space. You may also link to more resources.
-
-// For more examples, please refer to the Documentation
-
-// Roadmap
-// See the open issues for a list of proposed features (and known issues).
-
-// Contributing
-// Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are greatly appreciated.
-
-// Fork the Project
-// Create your Feature Branch (git checkout -b feature/AmazingFeature)
-// Commit your Changes (git commit -m 'Add some AmazingFeature')
-// Push to the Branch (git push origin feature/AmazingFeature)
-// Open a Pull Request
-// License
-// Distributed under the MIT License. See LICENSE for more information.
-
-// Contact
-// Your Name - @twitter_handle - email
-
-// Project Link: https://github.com/github_username/repo
-
-// Acknowledgements
-
-
-// <!-- PROJECT LOGO -->
-// <br />
-// <p align="center">
-//   <a href="https://github.com/github_username/repo">
-//     <img src="images/logo.png" alt="Logo" width="80" height="80">
-//   </a>
-
-//   <h3 align="center">YOUR_TITLE</h3>
-
-//   <p align="center">
-//     YOUR_SHORT_DESCRIPTION
-//     <br />
-//     <a href="https://github.com/github_username/repo"><strong>Explore the docs »</strong></a>
-//     <br />
-//     <br />
-//     <a href="https://github.com/github_username/repo">View Demo</a>
-//     ·
-//     <a href="https://github.com/github_username/repo/issues">Report Bug</a>
-//     ·
-//     <a href="https://github.com/github_username/repo/issues">Request Feature</a>
-//   </p>
-// </p>
-
-
-
-// <!-- TABLE OF CONTENTS -->
-// ## Table of Contents
-
-// * [About the Project](#about-the-project)
-//   * [Built With](#built-with)
-// * [Getting Started](#getting-started)
-//   * [Prerequisites](#prerequisites)
-//   * [Installation](#installation)
-// * [Usage](#usage)
-// * [Roadmap](#roadmap)
-// * [Contributing](#contributing)
-// * [License](#license)
-// * [Contact](#contact)
-// * [Acknowledgements](#acknowledgements)
-
-
-
-// <!-- ABOUT THE PROJECT -->
-// ## About The Project
-
-// [![Product Name Screen Shot][product-screenshot]](https://example.com)
-
-// Here's a blank template to get started:
-// **To avoid retyping too much info. Do a search and replace with your text editor for the following:**
-// `github_username`, `repo`, `twitter_handle`, `email`
-
-
-// ### Built With
-
-// * []()
-// * []()
-// * []()
-
-
-
-// <!-- GETTING STARTED -->
-// ## Getting Started
-
-// To get a local copy up and running follow these simple steps.
-
-// ### Prerequisites
-
-// This is an example of how to list things you need to use the software and how to install them.
-// * npm
-// ```sh
-// npm install npm@latest -g
-// ```
-
-// ### Installation
- 
-// 1. Clone the repo
-// ```sh
-// git clone https://github.com/github_username/repo.git
-// ```
-// 2. Install NPM packages
-// ```sh
-// npm install
-// ```
-
-
-
-// <!-- USAGE EXAMPLES -->
-// ## Usage
-
-// Use this space to show useful examples of how a project can be used. Additional screenshots, code examples and demos work well in this space. You may also link to more resources.
-
-// _For more examples, please refer to the [Documentation](https://example.com)_
-
-
-
-// <!-- ROADMAP -->
-// ## Roadmap
-
-// See the [open issues](https://github.com/github_username/repo/issues) for a list of proposed features (and known issues).
-
-
-
-// <!-- CONTRIBUTING -->
-// ## Contributing
-
-// Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
-
-// 1. Fork the Project
-// 2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
-// 3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
-// 4. Push to the Branch (`git push origin feature/AmazingFeature`)
-// 5. Open a Pull Request
-
-
-
-// <!-- LICENSE -->
-// ## License
-
-// Distributed under the MIT License. See `LICENSE` for more information.
-
-
-
-// <!-- CONTACT -->
-// ## Contact
-
-// Your Name - [@twitter_handle](https://twitter.com/twitter_handle) - email
-
-// Project Link: [https://github.com/github_username/repo](https://github.com/github_username/repo)
-
-
-
-// <!-- ACKNOWLEDGEMENTS -->
-// ## Acknowledgements
-
-// * []()
-// * []()
-// * []()
 
 
 
